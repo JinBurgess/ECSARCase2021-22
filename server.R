63382792742e1dd882a2eab85bab3a9184a98da7
Jin YuHan <jin.burgess19@ncf.edu>
2024-05-11 18:56:49 -0400

    Plotly of Sankey

--- server.R
+++ server.R
@@ -1,4 +1,4 @@
-# EC-SAR CASES SERVER
+# DATAVIS FINAL SERVER
 library(leaflet)
 library(ggplot2)
 library(plotly)
@@ -14,35 +14,25 @@ library(rgdal)
 library(networkD3)
 
 shinyServer(function(input, output, session) {
-  # caseFiles <- reactiveVal (NULL)
-  # missingDF <- reactiveVal(NULL)
-  # dataBase <- reactiveVal(NULL)
-  # requestList <- reactiveVal(NULL)
-  # 
-  # shinyjs::useShinyjs()
+  # react values---------------------------------------------------------------------------------
+  # caseFiles <- reactiveVal (totalFrame)
+  # missingDF <- reactiveVal(missingreacDF)
+  # dataBase <- reactiveVal(dataBase)
+  # requestList <- reactiveVal(requestList)
   activeButton <- reactiveVal(NULL)
+  # startDate <- reactiveVal(NULL)
+  # endDate <- reactiveVal(NULL)
   
-  
-  # DataSet ---------------------------------------------------------------------------------------------
+  # dataset ---------------------------------------------------------------------------------------------
   
   # determines if the user has uploaded any data into the system 
   dataFileUpload <- reactive({
-    viewDF <- caseFiles()%>%
+    caseFiles()%>%
       select(-c("psolcolors", "nodcolors"))
-    return(viewDF)  # Return the dataframe
-    # }
   }) # dataFileUpload
   
   # determines if the user has uploaded any data into the system 
   dataFileUpdate <- reactive({
-    # if (!is.null(input$caseFile)) {
-    # # Filters the csv file and creates a merged file that contains the updated coordinates for relative locations that 
-    # # can be mapped out. 
-    #   # reads in file from the input and creates heading since initial input doesn't have headers
-    #   fileSpec <- req(input$caseFile)
-    #   caseFolderReactive <- read.csv(fileSpec$datapath, header = FALSE, sep =  ',')
-    #   colnames(caseFolderReactive) = c("case", "date", "rlocation", "nod", "psol", "ssol", "ict")
-    #   
     # conducts validation and checking of data frames
     sortedData <- dataValidation(caseFiles()) # removes any entries that are not accepted by Pro Staff/ are not denoted in case form
     sortedData <- splitData(sortedData) # splitting data into entries with coordinates and those with only relative coordinates
@@ -61,14 +51,12 @@ shinyServer(function(input, output, session) {
     dataBase(dataBase)
     caseFiles(totalFrame)
     return(caseFiles())
-    # }
-    # return(list(totalFrame, missingDF))
   })
   
   # filters the dots on the map based on what the user has selected and updates map
   dataFilter <- reactive({
-    df <- dataFileUpdate()
-    if (nrow(df) > 0) {
+    df <- caseFiles()
+    if (nrow(df)>0) {
       df %>%
         filter(nod %in% input$caseDistress)
       
@@ -89,36 +77,6 @@ shinyServer(function(input, output, session) {
                "psol: primary solution<br/>",
                "ssol: secondary solution<br/>",
                "ict: initial call time<br/>"))
-  })
-  
-  # filters the dots on the map based on what the user has selected and updates map
-  dataFilter <- reactive({
-    df <- dataFileUpdate()
-    if (nrow(df) > 0) {
-      df %>%
-        filter(nod %in% input$caseDistress)
-
-    } else {df}
-  })
-  
-  # showing imported csv file before any data wrangle was done. 
-  output$dataset <- renderDataTable({
-    req(dataFileUpload())  # Ensure dataFile is not NULL
-    dataFileUpload()  # Get the dataframe
-  })
-  
-  output$ColumnInfo <- renderUI({
-    if (!is.null(input$caseFile)) {
-      HTML(paste("case: case number<br/>",
-                 "date: MM/DD/YYYY<br/>",
-                 "rlocation: relative location<br/>",
-                 "nod: nature of distress<br/>",
-                 "psol: primary solution<br/>",
-                 "ssol: secondary solution<br/>",
-                 "ict: initial call time<br/>"))
-    } else {
-      NULL  # Return NULL if no CSV file is loaded
-    }
   })
   
   # Analysis---------------------------------------------------------------------------------------------
@@ -148,8 +106,6 @@ shinyServer(function(input, output, session) {
     }
   }
   
-  plotToShow <- reactiveVal("none")
-  
   # Observe button clicks and update the plotToShow value
   observeEvent(input$assistancerendered, {
     setActiveButton("assistancerendered")
@@ -159,76 +115,79 @@ shinyServer(function(input, output, session) {
     setActiveButton("general")
   })
   
-  # # Render the Sankey graph
-  output$sangraph <- renderSankeyNetwork({
+  output$sangraph <- renderPlotly({
     links <- sankeynet(caseFiles())
     nodes <- data.frame(name = c(as.character(links$nod), as.character(links$psol)) %>% unique())
     links$IDnod <- match(links$nod, nodes$name) - 1
     links$IDsol <- match(links$psol, nodes$name) - 1
     
-    # If the overTime plot is already rendered, remove it
-    # shinyjs::disable("overTime")
-    
-    sankeyNetwork(Links = links, Nodes = nodes,
-                  Source = "IDnod", Target = "IDsol",
-                  Value = "value", NodeID = "name",
-                  sinksRight = FALSE)
+    plot_ly(
+      type = "sankey",
+      arrangement = "snap",
+      node = list(
+        label = nodes$name,
+        pad = 15,
+        thickness = 20,
+        line = list(color = "black", width = 0.5)
+      ),
+      link = list(
+        source = links$IDnod,
+        target = links$IDsol,
+        value = links$value
+      )
+    )
   })
-  # 
   # # Render the overtime plot
-  output$overTime <- renderPlot({
+  output$overTime <- renderPlotly({
     # Calculate the count of cases per day
     caseCount <- casePerDay(caseFiles())
     
-    # If the sangraph plot is already rendered, remove it
-    shinyjs::disable("sangraph")
-    
     # Plot the count of cases per day over time
-    ggplot(caseCount, aes(x = date, y = total)) +
-      geom_line() + geom_point() + theme_minimal()
+    plot <- ggplot(caseCount, aes(x = date, y = total)) +
+      geom_col()+ theme_minimal()
+    
+    ggplotly(plot)
     
   })
-  
   observeEvent(c(input$assistancerendered, input$general), {
     output$plot <- renderUI({
-      if(!is.null(activeButton())){
+      if (!is.null(activeButton())) {
         if (activeButton() == "assistancerendered") {
-          sankeyNetworkOutput("sangraph", width = "100%",  height = 650)
-        } else if (activeButton()  == "general") {
-          plotOutput("overTime", width = "100%", height = 650)
-        } 
-      }else {
-        HTML("This page will allow you to look a some Case Stats for the year 2021-2022")
+          plotlyOutput("sangraph", width = "100%",  height = 650)
+        } else if (activeButton() == "general") {
+          plotlyOutput("overTime", width = "100%", height = 650)
+        }
+      } else {
+        HTML("This page will allow you to look at some Case Stats for the year 2021-2022")
       }
     })
   })
   
-  # ECSARCases ---------------------------------------------------------------------------------------------
+  # EC-SAR Cases---------------------------------------------------------------------------------------------
   
-  # code to create the interactive map
+  # output$selectDates <- renderPrint({input$dates})
+  # 
+  # # code to create the interactive map
   output$ECSARCases <- renderLeaflet({
     # When there is no loaded csv file, it shows a blank map
-    if (is.null(dataFileUpdate())) {
+    if (is.null(dataFilter())) {
       leaflet() %>%
         addTiles() %>%
         setView(lng = -82.6,
                 lat = 27.7,
                 zoom = 9.5)
-    }
-    
-    # Shows map of cases within the totalFrame
-    else {
+    } else {
       leaflet() %>%
         addCircleMarkers(
           data = dataFilter(),
-          lat = ~ (as.numeric(Latitude)),
-          lng = ~ (as.numeric(Longitude)),
+          lat = ~ as.numeric(Latitude),
+          lng = ~ as.numeric(Longitude),
           color = ~ nodcolors,
           fillOpacity = 1.0,
           radius = 2,
           
-          # When the user clicks on a dot, it will give all the information about the case. 
-          # This information is the same that can be seen in dataTable tab. 
+          # When the user clicks on a dot, it will give all the information about the case.
+          # This information is the same that can be seen in dataTable tab.
           popup = paste(
             'Case Number: ', dataFilter()$case,'<br/>',
             'Date: ', dataFilter()$date,'<br/>',
@@ -236,120 +195,101 @@ shinyServer(function(input, output, session) {
             'Nature of Distress: ', dataFilter()$nod,'<br/>',
             'Primary Solution: ', dataFilter()$psol,'<br/>',
             'Secondary Solution: ', dataFilter()$ssol,'<br/>',
-            'Inital Call Time: ', dataFilter()$ict)) %>%
+            'Inital Call Time: ', dataFilter()$ict)
+        ) %>%
         addTiles() %>%
         addScaleBar() %>%
         setView(lng = -82.6,
                 lat = 27.7,
                 zoom = 9.5)
     }
-    
   })
   
-  # Setting GPS---------------------------------------------------------------------------------------------
+  
+  
+  # GPS ---------------------------------------------------------------------------------------------
   
   # reactive values to allow for user input and changes of data
-  output$genLocPicker <- renderUI({
-    if (!is.null(missingDF())) {
-      pickerInput(
-        inputId = 'genLoc',
-        label = "Select Entry",
-        choices = sort(unique(missingDF()$rlocation)),
-        options = list(`actions-box` = TRUE),
-        multiple = FALSE,
-        selected = unique(missingDF()$rlocation)
-      )
-    }
-  })
-  
-  rv_location <- reactiveValues(id = NULL, lat=NULL, lng=NULL)
-  
-  output$uiLat <- renderText({
-    if (!is.null(rv_location$lat)) {
-      HTML(paste('latitude :', rv_location$lat))
-    } else {
-      "Click on the map to get GPS Coordinates"  # Adjusted this line to use single string indexing
-    }
-  })
-  
-  output$uiLong <- renderText({
-    if (!is.null(rv_location$lng)) {
-      HTML(paste('Longitude :', rv_location$lng))
-    }
-  })
-  
-  output$clickMap <- renderLeaflet({
-    leaflet() %>%
-      addTiles() %>%
-      setView(lng = -82.6, lat = 27.7, zoom = 9.5)
-  })
-  
-  # When any click happens, identify clicks on map and log new location info.
-  observeEvent(input$clickMap_click,{
-    clickMap_info <- input$clickMap_click
-    
-    # Sets the Lat and long to the 3rd decimal place.
-      
-      rv_location$lat <- round(clickMap_info$lat,3)
-      rv_location$lng <- round(clickMap_info$lng,3)
-      
-    })
-  
-  # Add logging statements
-  observeEvent(input$saveButton, {
-    if(!is.null(input$caseFile)){
-      # Check if all required inputs are available
-      req(input$genLoc[1])
-      req(isolate(rv_location$lat))
-      req(isolate(rv_location$lng))
-      
-      # Remove the row from the missingDF because we have already found its GPS position
-      tempMissingDF <- filter(missingDF(), rlocation != input$genLoc[1])
-      
-      # Add the GPS position of the relative position to the dataBase
-      newData <- data.frame(
-        RLocation = isolate(input$genLoc[1]),
-        Latitude = isolate(rv_location$lat), 
-        Longitude = isolate(rv_location$lng)
-      )
-      dataBaseUpdated <- rbind(newData, dataBase())
-      
-      # Update the missingDF
-      write.csv(tempMissingDF, file = 'missingDF.csv', row.names = FALSE)
-      write.csv(dataBaseUpdated, 'RLocationLatLongs.csv', row.names = FALSE)
-      missingDF(tempMissingDF)
-      
-      # Update the dataframe used in the ECCase plot
-      updatedData <- dataFileUpdate()
-      
-      # Update the picker input choices
-      updatePickerInput(session, 
-                        'genLoc', 
-                        choices = if (nrow(tempMissingDF) > 0)
-                          sort(unique(tempMissingDF$rlocation)) else NULL
-      )
-      
-      # Return the updated dataBase
-      return(dataBaseUpdated)
-    }
-  })
-  
-  # Testing map to draw polygons
-  output$polygonMap <- renderLeaflet({
-    leaflet() %>%
-      addTiles() %>%
-      setView(lng = -82.6, lat = 27.7, zoom = 9.5) %>%
-      addDrawToolbar(
-        targetGroup='draw',
-        editOptions = editToolbarOptions(selectedPathOptions = selectedPathOptions())) 
-  })
-  
-  observeEvent(input$map_draw_new_feature, {
-    drawnFeatures <- input$map_draw_new_feature
-    drawnFeatureType <- drawnFeatures$type
-    if (drawnFeatureType == "polygon") {
-      drawnPolygon <- leaflet:::getGeoJSON(drawnFeatures)
-      polygonCoords <- leaflet:::getLatLngs(drawnPolygon)
-    }
-  })
+  # output$genLocPicker <- renderUI({
+  #   if (!is.null(missingDF())) {
+  #     pickerInput(
+  #       inputId = 'genLoc',
+  #       label = "Select Entry",
+  #       choices = sort(unique(missingDF()$rlocation)),
+  #       options = list(`actions-box` = TRUE),
+  #       multiple = FALSE,
+  #       selected = unique(missingDF()$rlocation)
+  #     )
+  #   }
+  # })
+  # 
+  # rv_location <- reactiveValues(id = NULL, lat=NULL, lng=NULL)
+  # 
+  # output$uiCoord <- renderText({
+  #   location_info <- reactiveValuesToList(rv_location)
+  #   
+  #   if (!all(is.null(unlist(location_info)))) {
+  #     HTML(paste('latitude :', location_info$lat),'<br/>', 
+  #          paste('longitude:', location_info$lng))
+  #   } else {
+  #     "Click on the map to see geological information"  # Adjusted this line to use single string indexing
+  #   }
+  # })
+  # 
+  # 
+  # output$clickMap <- renderLeaflet({
+  #   leaflet() %>%
+  #     addTiles() %>%
+  #     setView(lng = -82.6, lat = 27.7, zoom = 9.5)
+  # })
+  # 
+  # # When any click happens, identify clicks on map and log new location info.
+  # observeEvent(input$clickMap_click,{
+  #   clickMap_info <- input$clickMap_click
+  #   
+  #   # Sets the Lat and long to the 3rd decimal place.
+  #     
+  #     rv_location$lat <- round(clickMap_info$lat,3)
+  #     rv_location$lng <- round(clickMap_info$lng,3)
+  #     
+  #   })
+  # 
+  # # Add logging statements
+  # observeEvent(input$saveButton, {
+  #   print("Save button clicked")
+  #   
+  #   # Check if all required inputs are available
+  #   req(input$genLoc[1])
+  #   req(isolate(rv_location$lat))
+  #   req(isolate(rv_location$lng))
+  #   
+  #   # Remove the row from the missingDF because we have already found its GPS position
+  #   tempMissingDF <- filter(missingDF(), rlocation != input$genLoc[1])
+  #   
+  #   # Add the GPS position of the relative position to the dataBase
+  #   newData <- data.frame(
+  #     RLocation = isolate(input$genLoc[1]),
+  #     Latitude = isolate(rv_location$lat), 
+  #     Longitude = isolate(rv_location$lng)
+  #   )
+  #   dataBaseUpdated <- rbind(newData, dataBase())
+  #   
+  #   # Update the missingDF
+  #   write.csv(tempMissingDF, file = 'missingDF.csv', row.names = FALSE)
+  #   write.csv(dataBaseUpdated, 'RLocationLatLongs.csv', row.names = FALSE)
+  #   missingDF(tempMissingDF)
+  #   
+  #   # Update the dataframe used in the ECCase plot
+  #   updatedData <- dataFileUpdate()
+  #   
+  #   # Update the picker input choices
+  #   updatePickerInput(session, 
+  #                     'genLoc', 
+  #                     choices = if (nrow(tempMissingDF) > 0)
+  #                       sort(unique(tempMissingDF$rlocation)) else NULL
+  #   )
+  #   
+  #   # Return the updated dataBase
+  #   return(dataBaseUpdated)
+  # })
 }) # shinyServer
